4b775ae851a8eab0ce93374fc2bd2a87f470b821
MONY ALEXANDRA <amony001@enseirb-matmeca.fr>
2023-11-27 15:48:00 +0100

    preambule

--- Preambule/preambule.m
+++ Preambule/preambule.m
@@ -1,39 +1,43 @@
-clear; close all; clc;
 %% Sallmone Armela && Mony Alexandra | Groupe 5
+clear; close all; clc;
 
 %% Préambule à la méthode de traitement d un signal de parole bruité par un bruit blanc gaussien : procédure dite d’addition-recouvrement
 
-Fe = 8000;
-Te=1/Fe;
-T=20e-3;
-Lt=T/Te;
-% Les signaux de parole sont généralement supposés quasi stationnaires sur des durées de l'ordre de 20 à 40 millisecondes.
-
-% Charger le signal de parole (remplacez le chemin par le vôtre)
+%% Initialisation des paramètres
+% Charger le signal de parole 
 signal_parole=load('fcno03fz.mat');
 
+Fe = 8000; % Fréquence du signal
+Te=1/Fe;
+
+T=20e-3; % Durée d'une Trame
+Lt=T/Te; % Longueur d'une Trame 
+Ls=length(signal_parole.fcno03fz); %Longueur du signal
+nbT =floor(2*Ls/Lt - 2); % Nombre de Trame
+
+% Les signaux de parole sont généralement supposés quasi stationnaires sur des durées de l'ordre de 20 à 40 millisecondes.
+
+%% Traitement 
+
 % Bruiter le signal
-signal_bruite = bruiter_signal(signal_parole.fcno03fz, 20);
+RSB=20;
+signal_bruite = bruiter_signal(signal_parole.fcno03fz, RSB);
 
-recSignal=decomposition(signal_bruite,Lt);
+Trames=decomposition(signal_bruite,Lt,nbT);
+recSignal=reconstruction(Trames,Ls,Lt,nbT);
 
-figure;
-subplot(2,2,1);
-plot(signal_parole.fcno03fz,'r');
+%% Affichage
+subplot(2,1,1);
+plot(signal_parole.fcno03fz, 'r');
+title('Signal de parole original');
+xlabel('Temps');
+ylabel('Amplitude');
 
-
+subplot(2,1,2);
+plot(signal_bruite, 'r+');
 hold on;
-subplot(2,2,2);
-plot(signal_bruite,"r+");
-
-hold on;
-plot(recSignal,'kx');
-
-hold on;
-subplot(2,2,4);
-s=signal_bruite-recSignal';
-plot(s(240:1000),'b');
-
-
-
-
+plot(recSignal, 'kx');
+title('Signal de parole bruité et signal reconstitué');
+xlabel('Temps');
+ylabel('Amplitude');
+legend('Signal bruité', 'Signal reconstitué');
