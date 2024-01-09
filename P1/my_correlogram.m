1ab4622b039f3f7becba89f60fdf5fe07645560f
MONY ALEXANDRA <amony001@enseirb-matmeca.fr>
2024-01-09 14:11:29 +0100

    P1

--- P1/my_correlogram.m
+++
@@ -1,6 +0,0 @@
-function dsp_correlogram = my_correlogram(signal)
-    N = length(signal);
-    autocorr_signal = xcorr(signal, 'biased'); % Calcul de l'autocorrélation
-    dsp_correlogram = abs(fftshift(fft(autocorr_signal))); % DSP par transformée de Fourier
-    dsp_correlogram = dsp_correlogram(1:N); % Prendre la moitié du spectre
-end
--- P1/my_daniell.m
+++
@@ -1,6 +0,0 @@
-function dsp_daniell = my_daniell(signal,L)
-    % L: la largeur de la fenêtre
-    N = length(signal);
-    spectre_puissance = abs(fftshift(fft(signal))).^2 / N;
-    dsp_daniell = filter((1/L)*ones(1,L), 1, spectre_puissance);
-end
